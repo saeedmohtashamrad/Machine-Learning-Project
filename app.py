14d1e1f695b43c3f52217c31f87aa7933e139712
saeed <75488243+saeedmohtashamrad@users.noreply.github.com>
2022-08-16 01:32:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -103,9 +103,9 @@ acc = accuracy_score(y_test, y_pred)
 
 st.write(f'Classifier = {classifier_name}')
 st.write(f'Accuracy =', acc)
-st.write (f'precision = {precision_score(y_test, y_pred)}')
-st.write (f'recall = {recall_score(y_test, y_pred)}')
-st.write (f'f1_score = {f1_score(y_test, y_pred)}')
+st.write (f'Precision =', precision_score(y_test, y_pred))
+st.write (f'Recall =', recall_score(y_test, y_pred))
+st.write (f'F1_score =', f1_score(y_test, y_pred))
 
 #### Evaluation ####
 
