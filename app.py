ec06e30a192c166161157873ba5846ebd273dcb8
saeed <75488243+saeedmohtashamrad@users.noreply.github.com>
2022-08-16 01:04:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -108,6 +108,6 @@ print (classification_report(y_test, y_pred))
 
 
 cmx = confusion_matrix(y_test, y_pred)
-sns.heatmap(cmx, square= True, annot= True)
+fig = sns.heatmap(cmx, square= True, annot= True)
        
-#st.plt(fig)
+st.plt(fig)
